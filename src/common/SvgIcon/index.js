7c91b29695288ca02f6bfbb80e1486366790b061
Levi Liester <levi.liester@gmail.com>
2020-11-28 00:15:12 -0600

    use that sweet public URL

--- src/common/SvgIcon/index.js
+++ src/common/SvgIcon/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ReactSVG } from "react-svg";
 
-const SvgIcon = (props) => <ReactSVG {...props} src={`/img/svg/${props.src}`} />;
+const SvgIcon = (props) => <ReactSVG {...props} src={process.env.PUBLIC_URL+`/img/svg/${props.src}`} />;
 
 export default SvgIcon;
