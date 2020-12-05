3e4874ab025ce4bb1e5f1a3566d5139dc20d7e63
Levi Liester <levi.liester@gmail.com>
2020-12-05 00:05:03 -0600

    Adding search page just to see if it will work with the routing

---
+++ src/pages/Search/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ContentBlock from "../../components/ContentBlock";
+import Container from "../../common/Container";
+import ScrollToTop from "../../common/ScrollToTop";
+
+
+const Home = () => {
+  return (
+    <Container>
+      <ScrollToTop />
+      <ContentBlock
+        type="right"
+        first="true"
+        title={"Here is the future page for search"}
+        content={"This will be how people find providers"}
+        icon="developer.svg"
+      />
+
+    </Container>
+  );
+};
+
+export default Home;
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Home from "../pages/Home";
+import Search from "../pages/Search"
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Header />
       <Switch>
         <Route exact path={"/"} component={Home} />
+        <Route exact path={"/search"} component={Search} />
       </Switch>
       <Footer />
     </Suspense>
